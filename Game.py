2f35015710e63c22d9467d42a7fd5a13e5bd352b
Kyle Wisecarver <102608427+WisecarverWorks@users.noreply.github.com>
2022-05-18 19:42:48 -0500

    Created new game file. Created first number to name function. Created table for variable values.

---
+++ Game.py
@@ -0,0 +1,15 @@
+# 
+##
+### Rock Paper Scissors Lizard Spock
+###            Final
+##
+# 
+#   Before you begin coding, write an algorithm that represents the steps necessary to #   play a game of rock, paper, scissors, lizard, Spock in a best-of-three format. By #   writing out the steps, it will make you think about every piece needed to bring the #   game to life. Please submit to your instructor Slack channel once completed for  #   approval to start coding. Below is an example of how to get started:
+
+#  Step one: Display the rules of the game.
+#   Step Two: Ask for number of human users. 
+#    Step Three: Users select gesture
+#     Step Four: Compare and decide winner of round.
+#    Step Five: Decide winner one user reaches 2 rounds won. 
+#   Step Six: Display winner.  
+#  Step Seven: Restart prompt for new game. 
