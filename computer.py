556907f0f5f37777ca0fd48d43951ee33af1eac6
Kyle Wisecarver <102608427+WisecarverWorks@users.noreply.github.com>
2022-05-23 21:47:58 -0500

    feat: Created Computer file and class, created functions to select randomly from the list of gestures.

---
+++ computer.py
@@ -0,0 +1,14 @@
+# Import Player class and random
+import Player 
+from random import choice 
+
+class Computer(Player):
+    def __init__(self, name):
+        super().__init__(name)
+        self.listed_gestures = ["rock", "paper", "scissors", "lizard", "spock"]
+
+    def computer_choice(self):
+        self.computer_choice = choice(self.listed_gestures)
+
+    def round_won(self):
+        self.current_score += 1
